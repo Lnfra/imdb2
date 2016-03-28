476d3f2d11d2e20fc976ed2b3e45b16f636d4b82
la-github4WDI <lee.allan71@gmail.com>
2016-03-28 13:40:31 +0800

    revise movies and actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,29 @@
 class ActorsController < ApplicationController
 
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		Actor.create(params[:actor])		
+	end
+
+	def edit
+
+	end
+
+	def update
+	
+	end
+
+	private
+  
+    def actor_params
+      params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+    end
+
 end
